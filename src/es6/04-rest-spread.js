6d8a8ac91a715d00f619fc7635a8a8cb87a9c286
Adhemar <adhemarduran@gmail.com>
2022-10-09 14:26:47 -0400

    destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,12 @@
+//arrays destructuring
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, b);
+
+//Objec destructuring
+let user = {
+    username: 'Adhemar',
+    age: 35
+};
+let { username, age } = user;
+console.log(username, age);

cbbee1c7005ba40df2339c3657475fd6cdd79315
Adhemar <adhemarduran@gmail.com>
2022-10-09 14:36:48 -0400

    spread operator and rest

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,3 +10,21 @@ let user = {
 };
 let { username, age } = user;
 console.log(username, age);
+
+//spread operator
+let person = {
+    name: 'Adhemar',
+    age: 26
+};
+let country = 'MX';
+let data = { id: 1, ...person, country};
+console.log(data);
+
+//rest
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 3, 5, 6, 8);

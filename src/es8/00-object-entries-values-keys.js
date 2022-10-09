1584220207f34499a7ce95d76c044ad4865b7719
Adhemar <adhemarduran@gmail.com>
2022-10-09 19:14:48 -0400

    object entries, values and keys

---
+++ src/es8/00-object-entries-values-keys.js
@@ -0,0 +1,10 @@
+const countries = {
+    MX: 'Mexico',
+    CO: 'Colombia',
+    CE: 'Chile',
+    PE: 'Peru'
+};
+
+console.log(Object.entries(countries));
+console.log(Object.values(countries));
+console.log(Object.keys(countries));

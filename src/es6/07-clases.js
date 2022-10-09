4fc7bf5cb4106a43027af3573b3947ccaaeec5e5
Adhemar <adhemarduran@gmail.com>
2022-10-09 18:32:37 -0400

    clases

---
+++ src/es6/07-clases.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    speak() {
+        return 'Hello';
+    }
+    greeting() {
+        return `${this.speak()} ${this.name}`;
+    }
+
+    get uAge() {
+        return this.age;
+    }
+    set uAge(n) {
+        this.age = n;
+    }
+};
+
+const adhes = new User('Adhemar', 10);
+console.log(adhes.greeting());
+
+const bebeloper = new User('Jeremias', 20);
+console.log(bebeloper.greeting());
+
+console.log(adhes.uAge);
+console.log(adhes.uAge = 30);

09b1a7fcd100a4fefd78e81dbcd711caa09405f9
Adhemar <adhemarduran@gmail.com>
2022-10-10 19:38:00 -0400

    optional chaning

---
+++ src/es11/00-optional-chaning.js
@@ -0,0 +1,12 @@
+const users = {
+    adhes: {
+        country: 'MX'
+    },
+    Tefi: {
+        country: 'CO'
+    }
+}
+
+console.log(users.adhes.country);
+console.log(users.Tefi.age);
+console.log(users.maria?.country);
